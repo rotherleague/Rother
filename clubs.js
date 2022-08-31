4c17e348d7984220f25b6a0e8b8c5c412ee53be1
Rother Youth Football League <41952306+rotherleague@users.noreply.github.com>
2022-08-31 10:16:00 +0100

    Update clubs.js

--- clubs.js
+++ clubs.js
@@ -1,4 +1,8 @@
-
+     var qsRegex;
+     var buttonFilter;
+     var $quicksearch = $('#quicksearch');
+     var $container = $('#database')
+     var timeout;
 
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRF0IGMPN5alV0GPqtOE2QCJLgGb7X9QUFR6KiU--J61tKATgnOuo38kmueP5fWCVyDNz4jV4NMOjPo/pubhtml';
 
@@ -11,6 +15,89 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR
       }
 
       window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+     function showInfo(results) {
+       var data = results.data
+       var result = [];
+       var count = 1;
+       // data comes through as a simple array since simpleSheet is turned on
+       // alert("Successfully processed " + data.length + " rows!");
+       // console.log(data);
+		
+	   // loop to get the data from JSON and write it to the div's with the id's database and quicksearch 
+       $.each(data, function(i, v) {
+         // Parses the resulting JSON into the individual squares for each row
+         $container.append('<div id="element-item"><div class="category">' + v.Filter_category + '</div><img src="' + v.Pic_Link + '"><div class="name">' + v.Title + '</div><div class="boldsubhed">' + v.Location + '</div><div class="boldsubhed">' + v.City + '</div><div class="description">' + v.Date + '</div><div class="readmore"><a href="' + v.Website + ' " target="_blank">Website</a></div></div>');
+         // Gets all unique filtercategory values and puts them into an array
+         if ($.inArray(v.Filter_category, result) == -1) {
+           result.push(v.Filter_category);
+           // Creates the filter buttons
+           $('#filter').append('<button id="' + v.Filter_category + '" class="btn btn-default" data-value="choice' + count++ + '">' + v.Filter_category + '</button>')
+         }
+       });
+       // search function
+       $quicksearch.keyup(debounce(function() {
+         qsRegex = new RegExp($quicksearch.val(), 'gi');
+         $container.isotope();
+       }));
+       //  wait until images are loaded 
+       $container.imagesLoaded(function() {
+         // Sorts them into responsive square layout using isotope.js
+         $container.isotope({
+           itemSelector: '#element-item',
+           layoutMode: 'masonry',
+           // so that isotope will filter both search and filter results
+           filter: function() {
+             var $this = $(this);
+             var searchResult = qsRegex ? $this.text().match(qsRegex) : true;
+             var buttonResult = buttonFilter ? $this.is(buttonFilter) : true;
+             return searchResult && buttonResult;
+           }
+         });
+       });
+       // debounce so filtering doesn't happen every millisecond
+       function debounce(fn, threshold) {
+         return function debounced() {
+           if (timeout) {
+             clearTimeout(timeout);
+           }
+
+           function delayed() {
+             fn();
+             timeout = null;
+           }
+           timeout = setTimeout(delayed, threshold || 100);
+         }
+       }
+       // Adds a click function to all buttons in the group
+       $('.btn-group').each(function(i, buttonGroup) {
+         var $buttonGroup = $(buttonGroup);
+         var allbuttonids = $("button").attr('id');
+         $buttonGroup.on('click', 'button', function() {
+           // Changes to .is-checked class when clicked
+           $buttonGroup.find('.is-checked').removeClass('is-checked');
+           $(this).addClass('is-checked');
+           // Gets all values that matches the clicked button's data value
+           buttonFilter = $(this).attr('data-value');
+           textFilter = $(this).text();
+
+           function getitems() {
+             var name = $(this).find('.category').text();
+             if (textFilter != "Show All") {
+               return name.match(textFilter);
+             } else {
+               return "*";
+             }
+           }
+           buttonFilter = getitems || buttonFilter;
+           $container.isotope();
+         });
+       });
+     }
+
 
 
  function showInfo(results) {
