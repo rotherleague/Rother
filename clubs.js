6837a88f78b0cc0c2f0a6104abbeb78f468a9f6f
Rother Youth Football League <41952306+rotherleague@users.noreply.github.com>
2024-07-15 11:20:04 +0100

    Update clubs.js

--- clubs.js
+++ clubs.js
@@ -1,164 +1,88 @@
+var clubs_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR4VY_mJjSYwTf7n1xBWtsqvo3x4RIXErc4fhel4TRyEcAtG78pfsAfrLWTrXXTzigUJvT7nDUDWg0E/pubhtml';
+var teams_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQE9OKbCoSfPuNGoCPiKzoMwlZHKt7T5fWDPDxfGVSOJkPpjVl7pPvTNh34TBDbe2h0MX7SVRpJvg5X/pubhtml';
 
-// club
-//var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR4VY_mJjSYwTf7n1xBWtsqvo3x4RIXErc4fhel4TRyEcAtG78pfsAfrLWTrXXTzigUJvT7nDUDWg0E/pubhtml';
-// team
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQE9OKbCoSfPuNGoCPiKzoMwlZHKt7T5fWDPDxfGVSOJkPpjVl7pPvTNh34TBDbe2h0MX7SVRpJvg5X/pubhtml';
-
-
-      function init() {
-        Papa.parse(public_spreadsheet_url, {
-          download: true,
-          header: true,
-          complete: showInfo
-        })
-      }
-
-      window.addEventListener('DOMContentLoaded', init)
-
-
-
-     function showInfo(results) {
-       var data = results.data
-       var result = [];
-       var count = 1;
-       // data comes through as a simple array since simpleSheet is turned on
-       // alert("Successfully processed " + data.length + " rows!");
-       // console.log(data);
-		
-	   // loop to get the data from JSON and write it to the div's with the id's database and quicksearch 
-       $.each(data, function(i, v) {
-         // Parses the resulting JSON into the individual squares for each row
-         $container.append('<div id="element-item"><div class="category">' + v.Club_Name + '</div><div class="name">' + v.First_Name + '</div><div class="boldsubhed">' + v.Ground_Postcode + '</div><div class="boldsubhed">' + v.Ground_Name + '</div><div class="description">' + v.Last_Name + '</div><div class="readmore">' + v.Age_Group + '</div></div>');
-         // Gets all unique filtercategory values and puts them into an array
-         if ($.inArray(v.Filter_category, result) == -1) {
-           result.push(v.Filter_category);
-           // Creates the filter buttons
-           $('#filter').append('<div id="' + v.Email + '" class="btn btn-default" data-value="choice' + count++ + '">' + v.Email + '</div>')
-         }
-       });
-      
-      
-     }
-
-
-
- function showInfo2(results) {
-        var data = results.data
-
-        var cat = '';
-        var cat_li = '';
-        var list_li = '';
-        var content_li = '';
-        var nav_li = '';
-        var navlist_li = '';
-
-        var i = 4;
-
-        /*
-        $("#table_info").text("We found the tables " + data.model_names.join(", "));
-
-        $.each( data.sheets(), function(i, sheet) {
-        $("#table_info").append("<p>" + sheet.name + " has " + sheet.column_names.join(", ") + "</p>");
-        });
-        */
-       $.each( data.sheets("clubs").all(), function(i, clubs) {
-
-            nav_li = $('<div></div>');
-
-            if (clubs.clubname != 'Name Of Club'){
-
-                navlist_li = '<a href="#ref'+ clubs.clubname.replace(/\s/g,'-').toLowerCase() +'" class="nav-link">'+ clubs.clubname +'</a>';
-          
-            }
-           
-            if (clubs.visible.toLowerCase() === "show"){ 
-                nav_li.append('<li class="list-group-item p-0">'+ navlist_li + '</li>');
-            }    
-
-                
-             //Combine content
-             nav_li.appendTo("#nav");
-
-             navlist_li = '';
-
-        });
-
-        $.each( data.sheets("clubs").all(), function(i, clubs) {
-
-            // Show category
-            
-            // link to the clubs
-            list_li = $('<div></div>');
-
-            
-
-            if (clubs.clubname != 'Name Of Club'){
-
-            
-                if  (cat != clubs.clubname){
-                    
-                    cat_li = '<br><div class="list-group-item list-group-item-action active" id="ref'+ clubs.clubname.replace(/\s/g,'-').toLowerCase() +'">'+
-                    '<div class="d-flex w-100 justify-content-between">'+
-                            '<h1>' + clubs.clubname +'</h1>' +
-                        '</div>' +
-                    '</div>';
-                }
-
-                /*
-                if (map.description != ''){
-                    content_li ='<p class="mb-1">' +map.agegroupinwhichteamwillparticipatethisseason.replace(/\n/g, "<br />") + '</p>';
-                }
-                */
-
-                //Manager Details
-                content_li = content_li +'<strong>Club Secretary:</strong> ';
-                
-                if (clubs.firstname != ''){
-                    content_li = content_li  + clubs.firstname + ' '+ clubs.lastname + '<br>';
-                }
-
-                // only show ticked items
-                if (clubs.visible.toLowerCase() === "show"){
-                    if (clubs.mobile != ''){
-                        content_li = content_li +'<strong>Tel:</strong> <a href="tel:'+clubs.mobile.replace(" ", "")+'">' + clubs.mobile + '</a><br>';
-                    }
-                    if (clubs.email != ''){
-                        content_li = content_li +'<strong>Email:</strong> <a href="mailto:'+clubs.email+'">' + clubs.email + '</a><br>';
-                    }
-                }
-
-               
-
-                //Combine content
-                list_li.append('<div class="list-group-item list-group-item-action">'+ '<div  class="d-flex w-100 justify-content-between">'+ '<h5 class="mb-1">' + clubs.clubname  +'</h5></div>'+ content_li + '</div>');
-                
-                list_li.prepend(cat_li);
-
-                list_li.appendTo("#clubs");
-
-                
-            }
-    
-            // Set current category for loop
-            cat = clubs.clubname;
-            cat_team = '';
-            cat_li = '';
-            content_li = '';
-
-            
-        });
-
+function init() {
+  Papa.parse(clubs_spreadsheet_url, {
+    download: true,
+    header: true,
+    complete: function (clubsResults) {
+      Papa.parse(teams_spreadsheet_url, {
+        download: true,
+        header: true,
+        complete: function (teamsResults) {
+          showInfo(clubsResults, teamsResults);
+        }
+      });
+    }
+  });
 }
 
+window.addEventListener('DOMContentLoaded', init);
 
+function showInfo(clubsResults, teamsResults) {
+  var clubsData = clubsResults.data;
+  var teamsData = teamsResults.data;
 
-function titleCase(str){
-   str = str.toLowerCase().split(' ');
-   let final = [];
+  var $nav = $('#nav');
+  var $clubs = $('#clubs');
 
-    for(let  word of str){
-      final.push(word.charAt(0).toUpperCase()+ word.slice(1));
+  var cat = '';
+  var content_li = '';
+
+  $.each(clubsData, function (i, club) {
+    if (club['Name Of Club'] !== 'Name Of Club') {
+      var nav_li = $('<div></div>');
+      var navlist_li = '';
+
+      if (club['Visible'].toLowerCase() === 'show') {
+        navlist_li = '<a href="#ref' + club['Name Of Club'].replace(/\s/g, '-').toLowerCase() + '" class="nav-link">' + club['Name Of Club'] + '</a>';
+        nav_li.append('<li class="list-group-item p-0">' + navlist_li + '</li>');
+        nav_li.appendTo($nav);
+      }
+
+      if (cat !== club['Name Of Club']) {
+        var cat_li = '<br><div class="list-group-item list-group-item-action active" id="ref' + club['Name Of Club'].replace(/\s/g, '-').toLowerCase() + '">' +
+          '<div class="d-flex w-100 justify-content-between">' +
+          '<h1>' + club['Name Of Club'] + '</h1>' +
+          '</div>' +
+          '</div>';
+        content_li += cat_li;
+      }
+
+      content_li += '<strong>Club Secretary:</strong> ' + club['First Name'] + ' ' + club['Surname'] + '<br>';
+      if (club['Visible'].toLowerCase() === 'show') {
+        if (club['Mobile']) {
+          content_li += '<strong>Tel:</strong> <a href="tel:' + club['Mobile'].replace(/\s/g, '') + '">' + club['Mobile'] + '</a><br>';
+        }
+        if (club['Email']) {
+          content_li += '<strong>Email:</strong> <a href="mailto:' + club['Email'] + '">' + club['Email'] + '</a><br>';
+        }
+      }
+
+      // Add teams for this club
+      content_li += '<h3>Teams</h3>';
+      var teamsList = teamsData.filter(team => team['Club Name'] === club['Name Of Club']);
+      if (teamsList.length > 0) {
+        content_li += '<ul>';
+        teamsList.forEach(team => {
+          content_li += '<li>' + team['Name Of Team'] + ' (' + team['Age Group in which team will participate this season'] + ')</li>';
+        });
+        content_li += '</ul>';
+      } else {
+        content_li += '<p>No teams available</p>';
+      }
+
+      var list_li = $('<div></div>');
+      list_li.append('<div class="list-group-item list-group-item-action"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">' + club['Name Of Club'] + '</h5></div>' + content_li + '</div>');
+      list_li.prepend(cat_li);
+      list_li.appendTo($clubs);
+
+      cat = club['Name Of Club'];
+      content_li = '';
     }
+  });
+}
 
-  return final.join(' ')
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
